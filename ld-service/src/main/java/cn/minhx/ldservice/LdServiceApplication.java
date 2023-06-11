efa9bd67abf3461497560bf2a49a87c5d037d659
Minh-X <746618739@qq.com>
2023-06-12 02:24:54 +0800

    1.service模块创建 2.依赖关系调整

---
+++ ld-service/src/main/java/cn/minhx/ldservice/LdServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.minhx.ldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdServiceApplication.class, args);
+    }
+
+}
---
+++ ld-service/src/test/java/cn/minhx/ldservice/LdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.minhx.ldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
