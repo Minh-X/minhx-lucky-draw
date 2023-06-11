f170462211058090a4bab71344817ebd4d57210d
Minh-X <746618739@qq.com>
2023-06-12 01:26:51 +0800

    gateway模块创建

---
+++ ld-gateway/src/main/java/cn/minhx/ldgateway/LdGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.minhx.ldgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdGatewayApplication.class, args);
+    }
+
+}
