47ab0d67a194ad0e1b5f2043d4a8d6d2909dcca1
Minh-X <746618739@qq.com>
2023-06-12 01:02:41 +0800

    base模块创建

---
+++ ld-base/ld-base-common/src/main/java/cn/minhx/common/AutoLdBaseCommon.java
@@ -0,0 +1,8 @@
+package cn.minhx.common;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"cn.minhx.common", "cn.minhx.config"})
+public class AutoLdBaseCommon {
+
+}
