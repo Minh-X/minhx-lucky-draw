c425ad3bea20148855245339b3b27d87b8ce98a3
Minh-X <746618739@qq.com>
2023-06-12 23:19:45 +0800

    自动装配

--- ld-base/ld-base-common/src/main/java/cn/minhx/common/AutoLdBaseCommon.java
+++ ld-base/ld-base-common/src/main/java/cn/minhx/common/AutoLdBaseCommon.java
@@ -1,7 +1,9 @@
 package cn.minhx.common;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ComponentScan(basePackages = {"cn.minhx.common", "cn.minhx.config"})
 public class AutoLdBaseCommon {
 
